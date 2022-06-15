2375cfe3a5ea3a6af0345e05effbe75e21cbed16
marc-szymkowiak <57388720+marc-szymkowiak@users.noreply.github.com>
2022-06-15 13:48:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Simple Demonstrator for applying at copycat
+# Simple Demonstrator for applying at ConfigCat
 # written by Marc Szymkowiak June 2022
 
 import configcatclient
@@ -13,14 +13,14 @@ if __name__ == '__main__':
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
 
-    print("Just a quick demonstrator to show the features ob Copycats' feature flag system")
+    print("Just a quick demonstrator to show the features ob ConfigCats' feature flag system")
     print("You can enable/disable network informations via the feature flag used:")
-    print(f"Systemtype and CPU used in your system: {system.system} on {system.processor}")
-    print(f"Architecture of your current system: {system.machine}")
+    print(f"Type and CPU used in your system: {system.system} on {system.processor}")
+    print(f"Architecture of your system: {system.machine}")
 if configcat_client.get_value('isNetworkInfoEnabled', False):
     print("Your machines hostname: " + hostname)
     print("IP address: " + ip)
-    print("MAC address of the used netword adapter: ", end="")
+    print("MAC address of the used network adapter: ", end="")
     print(':'.join(re.findall('..', '%012x' % uuid.getnode())))
 
     configcat_client.stop()
